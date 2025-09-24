e3a02e9821adad581483afd77309c372773b242c
karolcr22 <130563021+karolcr22@users.noreply.github.com>
2025-09-23 20:55:06 -0500

    Edición javascript

--- challenge-amigo-secreto-alura/app.js
+++ challenge-amigo-secreto-alura/app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigosecreto = []
+
+
+
+function agregarAmigoSecreto(){
+    let nombreDelAmigoSecreto = document.getElementById('amigo').value;
+
+    if(nombreDelAmigoSecreto === null || nombreDelAmigoSecreto === ""){
+        alert("Por favor, inserte un nombre.");
+    } else{
+        amigos.push(nombreDelAmigoSecreto);
+        console.log("Amigo agregado:", nombreDelAmigoSecreto);
+        console.log("Lista actual de amigos:", amigosecreto);
+    }
+    
+    actualizaListaAmigos();
+    limpiarCaja();
+   
+    
+}
